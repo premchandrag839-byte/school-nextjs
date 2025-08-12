1f48e913274e562dee89c90a8993c5179b330663
premchandrag839-byte <premchandrag839@gmail.com>
2025-08-12 17:17:25 +0530

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,11 +1,12 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "PROJECT_ID", // <-- apna project id yaha daalo
-      token: "5iqtFgsEdh4iNNfiApHk12OunkmcvPdwQ8T4QzFQk9XgSDsW8BATHymMpAAfcXTze6na9cO3UXOzbOwrJw"
+      id: "wWHyaBbW9UpmgJ6WJ21M8L",  // ID of a project you are using
+      token: "5iqtFgsEdh4iNNfiApHk12OunkmcvPdwQ8T4QzFQk9XgSDsW8BATHymMpAAfcXTze6na9cO3UXOzbOwrJw"  // API token for that project
     }
   ],
-  preview: true
-});
+  // Fetches the latest revisions, whether or not they were unpublished!
+  // Disable for production to ensure you render only published changes.
+  preview: true,
+})
